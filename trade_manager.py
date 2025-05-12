46a9fe6bba343ecd4e3e587a17f9abaa1c1a1815
aulakhjatt302 <aulakhjatt302@gmail.com>
2025-05-12 03:07:19 +0100

    Update trade_manager.py

--- trade_manager.py
+++ trade_manager.py
@@ -1,28 +1,32 @@
 import os
-from pybit import usdt_perpetual
+from pybit.unified_trading import HTTP
 
 BYBIT_API_KEY = os.getenv("BYBIT_API_KEY")
 BYBIT_API_SECRET = os.getenv("BYBIT_API_SECRET")
 
-session = usdt_perpetual.HTTP(
-    endpoint="https://api.bybit.com",
+session = HTTP(
+    testnet=False,
     api_key=BYBIT_API_KEY,
     api_secret=BYBIT_API_SECRET
 )
 
 def execute_trade(signal):
     symbol = signal['symbol']
-    side = signal['side']
+    side = signal['side'].upper()
     qty = 25
     leverage = 10
-    session.set_leverage(symbol=symbol, buy_leverage=leverage, sell_leverage=leverage)
-    order = session.place_active_order(
+
+    # Set leverage
+    session.set_leverage(category="linear", symbol=symbol, buyLeverage=leverage, sellLeverage=leverage)
+
+    # Place order
+    order = session.place_order(
+        category="linear",
         symbol=symbol,
         side=side,
-        order_type="Market",
+        orderType="Market",
         qty=qty,
-        time_in_force="GoodTillCancel",
-        reduce_only=False,
-        close_on_trigger=False
+        timeInForce="GoodTillCancel"
     )
-    print("Order:", order)
+
+    print("Order response:", order)
